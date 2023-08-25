4e037daf88d6275bfa02463ba8b52c5da69c441e
Jose Zaccaro <a.zaccaro42@gmail.com>
2023-08-25 18:53:34 -0300

    fix btn

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,38 +1,38 @@
 import { Link } from 'react-router-dom';
 import ReactiveButton from 'reactive-button';
 
-    function CallToActionButton({toTop}) {
-    
+function CallToActionButton({ toTop }) {
+
 
     return (<>
-    <Link to="/cities" className="call-to-action">
+        <Link to="/cities" className="call-to-action">
 
-        <div className="adventure-btn-ctn">
-        <ReactiveButton 
-            color={'teal'}
-            idleText={"YOUR ADVENTURE STARTS HERE!"}
-            type={'button'}
-            className={'class1 class2'}
-            style={{ borderRadius: '5px' }}
-            outline={true}
-            shadow={true}
-            rounded={true}
-            size={'large'}
-            block={false}
-            buttonRef={null}
-            width={"30vw"}
-            height={"10vh"} 
+            <div className="adventure-btn-ctn">
+                <ReactiveButton
+                    color={'teal'}
+                    idleText={"YOUR ADVENTURE STARTS HERE!"}
+                    type={'button'}
+                    className={'class1 class2'}
+                    style={{ borderRadius: '5px' }}
+                    outline={true}
+                    shadow={true}
+                    rounded={true}
+                    size={'large'}
+                    block={false}
+                    buttonRef={null}
+                    width={"30vw"}
+                    height={"10vh"}
 
-            onClick={toTop}
-            />
+                    onClick={toTop}
+                />
             </div>
-            
-         <div className="click-here"style={{backgroundImage:"url('./assets/clickhere.png')", backgroundSize:'contain'}} 
-         onClick={toTop}
-         />
-    </Link>
 
-        </>
+            <div className="click-here" style={{ backgroundImage: "url('./assets/clickhere.png')", backgroundSize: 'contain' }}
+                onClick={toTop}
+            />
+        </Link>
+
+    </>
     );
 }
 
